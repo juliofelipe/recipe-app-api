c671b4bd24546e9d7babed98165f4c31c54ab010
Julio Felipe <julio@informach.com.br>
2022-06-04 15:41:13 -0300

    feat: add tag and ingredient

--- app/core/admin.py
+++ app/core/admin.py
@@ -45,4 +45,7 @@ class UserAdmin(BaseUserAdmin):
    
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
 
